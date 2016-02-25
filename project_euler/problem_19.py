0c6fa0b3fd2a41a9f375730eac3d0a060976b85e
Sergei-Rudenkov <Sergei_Rudenkov@epam.com>
2016-02-25 14:19:58 +0300

    init euler

---
+++ project_euler/problem_19.py
@@ -0,0 +1,3 @@
+
+#How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+#Год не является високосным, если он не кратен 4, либо он кратен 100, но при этом не кратен 400.
